0eb0cab8e679b3b9b519b8d73654469cfdde56bd
ClaireKth <claire.kurth@orange.fr>
2024-12-10 09:51:26 +0100

    tailwind + routeur

---
+++ src/routes/Router.js
@@ -0,0 +1,12 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import HelloWorld from "../components/HelloWorld.vue";
+
+const routes = [{ path: "/", component: HelloWorld }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
